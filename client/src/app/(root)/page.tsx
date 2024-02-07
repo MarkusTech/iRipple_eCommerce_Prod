aa94a80c699b6b1edbd4d178c4735b47ad4bfc91
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-07 09:22:59 +0800

    Update page.tsx

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
       <Button>Save</Button>
     </div>
   );
-}
+};
+
+export default SetupPage;
