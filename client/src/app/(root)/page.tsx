31cd3e9d8c99cb0cdfa2005ba546e21a0134eddc
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-07 11:03:09 +0800

    Update page.tsx

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -1,15 +1,20 @@
-"use clien";
+"use client";
 
-import { Modal } from "@/components/ui/modal";
-import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+import { useStoreModal } from "../../../hooks/use-store-modal";
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <UserButton />
-      Root Page
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 };
 
 export default SetupPage;
