dab71b5755f06e851698a0c7c977c094cdbbe9ab
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-09 11:17:48 +0800

    navbar

---
+++ client/src/components/main-nav.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export function MainNav({
+  children,
+  ...props
+}: React.HtmlHTMLAttributes<HTMLElement>) {
+  return (
+    <div>
+      <p>Main Navigation</p>
+    </div>
+  );
+}
