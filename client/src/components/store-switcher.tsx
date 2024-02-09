a42840b294f23d3800f15ae78467665857346e4b
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-09 11:55:12 +0800

    Update store-switcher.tsx

--- client/src/components/store-switcher.tsx
+++ client/src/components/store-switcher.tsx
@@ -1,4 +1,21 @@
-export default function StoreSwticher() {
+"use client";
+
+import { Store } from "@prisma/client";
+
+import { PopoverTrigger } from "./ui/popover";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
+
+export default function StoreSwticher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
   return (
     <div>
       <div>Store Switcher!</div>
