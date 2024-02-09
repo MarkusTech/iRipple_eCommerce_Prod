53a689f22a0dd14662623f6c08652dcb585e50aa
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-09 11:03:01 +0800

    Update page.tsx

--- client/src/app/(dashboard)/[storeid]/(routes)/page.tsx
+++ client/src/app/(dashboard)/[storeid]/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const DashboardPage = () => {
-  return <div>Dashboard</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: { storedId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storedId,
+    },
+  });
+  return (
+    <div>
+      <div>Active Store: {store?.name}</div>
+    </div>
+  );
 };
 
 export default DashboardPage;
