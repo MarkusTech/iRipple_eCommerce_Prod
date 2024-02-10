d9ca44011bc4745081f6fa26751d2705bfa7e64a
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-10 09:13:42 +0800

    Update navbar.tsx

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,19 +1,32 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-import { MainNav } from "./main-nav";
-import StoreSwticher from "./store-switcher";
+import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
+// import { ThemeToggle } from "@/components/theme-toggle";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwticher />
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
-        <div className="ml-auto flex items-center space-x-4 ">
-          <UserButton
-            afterSignOutUrl="/client
-          "
-          />
+        <div className="ml-auto flex items-center space-x-4">
+          {/* <ThemeToggle /> */}
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
