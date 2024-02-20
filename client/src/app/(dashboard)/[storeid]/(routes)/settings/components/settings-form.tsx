dee669cdf451159f93a2c91e9067810bd4e65635
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-20 09:16:09 +0800

    asd

---
+++ client/src/app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,7 @@
+export const SettingsForm = () => {
+  return (
+    <div>
+      <div>Settings Form</div>
+    </div>
+  );
+};
--- client/src/app/(dashboard)/[storeid]/(routes)/settings/page.tsx
+++ client/src/app/(dashboard)/[storeid]/(routes)/settings/page.tsx
@@ -2,6 +2,8 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { SettingsForm } from "./components/settings-form";
+
 interface SettingsPageProps {
   params: {
     storeId: string;
@@ -28,7 +30,9 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
 
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y p-8 pt-6">Hello Settings!</div>
+      <div className="flex-1 space-y p-8 pt-6">
+        <SettingsForm />
+      </div>
     </div>
   );
 };
